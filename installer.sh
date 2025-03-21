c581aa495ec5d6b4fbfa628ccc34638166f823f0
NinIcaty <136358167+NinIcaty@users.noreply.github.com>
2025-03-21 16:15:46 +0530

    Update installer.sh
    Checks if the Direcrory "pycraft" already exsists if so it stops

--- installer.sh
+++ installer.sh
@@ -1,15 +1,20 @@
-exh Starting Download
+DIR="pycraft"
 if command -v python3 >/dev/null 2>&1; then
-    echo Starting install
-    git clone https://github.com/NinIcaty/pycraft
-    echo Cloned repo
-    cd pycraft
-    python -m venv venv
-    echo Venv Created
-    source venv/bin/activate
-    echo Activated
+    if [ -d "$DIR" ]; then
+        echo "Please delete the Directory pycraft"
+        exit 1
+    else
+        echo "Directory does not exist, continuing..."
+        echo Starting install
+        git clone https://github.com/NinIcaty/pycraft
+        echo Cloned repo
+        cd pycraft
+        python -m venv venv
+        echo Venv Created
+        source venv/bin/activate
+        echo Activated
 
-
+    fi
 else
     echo "Python 3 is not installed,Please install it."
 fi
